14ea02b538db54eef1fb0085d8415f18c4c6e3c0
YuqinGuan <yu8160gu-s@student.lu.se>
2022-03-03 20:26:21 +0100

    fake movielist and show movies in watchlist

---
+++ src/app/mockFilmList.ts
@@ -0,0 +1,15 @@
+export interface Movie {
+    id: number;
+    name: string;
+}
+
+export const movies= [
+ {
+    id:1
+    name: "Skyfall"
+ },
+ {
+     id:2
+     name:"Avenger"
+ }
+]
--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {movies} from "../mockFilmList.ts"
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.css']
 })
 export class WatchlistComponent implements OnInit {
-
-  constructor() { }
+  movies;
+  constructor() { 
+     this.movies = movies;
+  }
 
   ngOnInit(): void {
+  }
+
+  addNotes(){
+    //todo
   }
 
 }
