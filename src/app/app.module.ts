f561e6b4b886ed9e19d675aad8141da7d5a55944
JackeJacob <ja0032kr-s@student.lu.se>
2022-03-03 15:22:02 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { ViewFilmComponent } from './view-film/view-film.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WatchlistComponent,
+    ViewFilmComponent,
+    NavigationBarComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule
